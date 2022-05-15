7332a229cc848dab5a5afa6b4ea44a280beff35e
Ekaterina <brekhova.katya@gmail.com>
2022-05-15 14:12:15 +0300

    Write tests

---
+++ __tests__/contentDisplay.test.js
@@ -0,0 +1,107 @@
+import {
+  test,
+  expect,
+  beforeEach,
+  beforeAll,
+} from '@jest/globals';
+import '@testing-library/jest-dom';
+import testingLibrary from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import nock from 'nock';
+import init from '../src/init.js';
+import i18nextCreateInstance from '../lib/i18next.js';
+
+const { screen, waitFor } = testingLibrary;
+const user = userEvent.setup();
+const i18nextInstance = await i18nextCreateInstance();
+
+nock.disableNetConnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let elements;
+let rssData;
+
+beforeAll(() => {
+  const pathToRssData = path.join(__dirname, '..', '__fixtures__', 'rssDataExample.xml');
+  rssData = fs.readFileSync(pathToRssData).toString();
+
+  nock('https://allorigins.hexlet.app')
+    .get('/get')
+    .query({
+      url: 'https://example.com/news',
+      disableCache: true,
+    })
+    .reply(404, 'Not Found');
+
+  nock('https://allorigins.hexlet.app')
+    .get('/get')
+    .query({
+      url: 'https://lorem-rss.herokuapp.com/feed',
+      disableCache: true,
+    })
+    .reply(200, { contents: rssData });
+});
+
+beforeEach(() => {
+  const pathToRssData = path.join(__dirname, '..', '__fixtures__', 'rssDataExample.xml');
+  rssData = fs.readFileSync(pathToRssData).toString();
+
+  const pathToHtml = path.join(__dirname, '..', '__fixtures__', 'index.html');
+  const initHtml = fs.readFileSync(pathToHtml).toString();
+  document.body.innerHTML = initHtml;
+
+  init(i18nextInstance);
+
+  elements = {
+    submitBtn: screen.getByText(/Добавить/),
+    rssInput: screen.getByLabelText(/Ссылка RSS/),
+    feeds: screen.getByTestId('feeds'),
+    posts: screen.getByTestId('posts'),
+  };
+});
+
+test('state without any content', async () => {
+  expect(elements.feeds).toBeEmptyDOMElement();
+  expect(elements.posts).toBeEmptyDOMElement();
+});
+
+test('dysplay content on requests', async () => {
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.feeds).toBeEmptyDOMElement();
+    expect(elements.posts).toBeEmptyDOMElement();
+  });
+
+  await user.type(elements.rssInput, 'Petya');
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.feeds).toBeEmptyDOMElement();
+    expect(elements.posts).toBeEmptyDOMElement();
+  });
+
+  await user.clear(elements.rssInput);
+  await user.type(elements.rssInput, 'https://example.com/news');
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.feeds).toBeEmptyDOMElement();
+    expect(elements.posts).toBeEmptyDOMElement();
+  });
+
+  await user.clear(elements.rssInput);
+  await user.type(elements.rssInput, 'https://lorem-rss.herokuapp.com/feed');
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.feeds).toHaveTextContent('Фиды');
+    expect(elements.feeds).toHaveTextContent('Lorem ipsum feed for an interval of 1 minutes with 10 item(s)');
+    expect(elements.posts).toHaveTextContent('Посты');
+  });
+});
--- __tests__/rssForm.test.js
+++ __tests__/rssForm.test.js
@@ -1,4 +1,9 @@
-import { test, expect, beforeEach } from '@jest/globals';
+import {
+  test,
+  expect,
+  beforeEach,
+  beforeAll,
+} from '@jest/globals';
 import '@testing-library/jest-dom';
 import testingLibrary from '@testing-library/dom';
 import userEvent from '@testing-library/user-event';
@@ -19,11 +24,37 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let elements;
+let rssData;
+
+beforeAll(() => {
+  const pathToRssData = path.join(__dirname, '..', '__fixtures__', 'rssDataExample.xml');
+  rssData = fs.readFileSync(pathToRssData).toString();
+
+  nock('https://allorigins.hexlet.app')
+    .get('/get')
+    .query({
+      url: 'https://example.com/news',
+      disableCache: true,
+    })
+    .reply(404, 'Not Found');
+
+  nock('https://allorigins.hexlet.app')
+    .get('/get')
+    .query({
+      url: 'https://lorem-rss.herokuapp.com/feed',
+      disableCache: true,
+    })
+    .reply(200, { contents: rssData });
+});
 
 beforeEach(() => {
-  const pathToFixture = path.join(__dirname, '..', '__fixtures__', 'index.html');
-  const initHtml = fs.readFileSync(pathToFixture).toString();
+  const pathToRssData = path.join(__dirname, '..', '__fixtures__', 'rssDataExample.xml');
+  rssData = fs.readFileSync(pathToRssData).toString();
+
+  const pathToHtml = path.join(__dirname, '..', '__fixtures__', 'index.html');
+  const initHtml = fs.readFileSync(pathToHtml).toString();
   document.body.innerHTML = initHtml;
+
   init(i18nextInstance);
 
   elements = {
@@ -40,52 +71,69 @@ test('fresh rss form', async () => {
   expect(elements.rssInput).not.toHaveClass('border', 'border-danger', 'is-invalid');
   expect(elements.feedbackContainer).not.toHaveTextContent();
   expect(elements.feedbackContainer).not.toHaveClass('text-danger');
-});
-
-test('validation works', async () => {
   expect(screen.queryByText('Ссылка должна быть валидным URL')).not.toBeInTheDocument();
   expect(screen.queryByText('RSS уже существует')).not.toBeInTheDocument();
-  expect(screen.queryByText('Это обязательное поле')).not.toBeInTheDocument();
+  expect(screen.queryByText('Ресурс не содержит валидный RSS')).not.toBeInTheDocument();
+  expect(screen.queryByText('Не должно быть пустым')).not.toBeInTheDocument();
+  expect(screen.queryByText('RSS успешно загружен')).not.toBeInTheDocument();
+});
 
+test('rss form validation', async () => {
   await user.click(elements.submitBtn);
-
-  expect(elements.submitBtn).toBeEnabled();
-  expect(elements.rssInput).toHaveValue('');
-  expect(elements.rssInput).toHaveClass('border', 'border-danger', 'is-invalid');
-  expect(elements.feedbackContainer).toHaveTextContent('Это обязательное поле');
-  expect(elements.feedbackContainer).toHaveClass('text-danger');
-
-  await user.type(elements.rssInput, 'Petya');
-  console.log(elements.rssInput.value);
-  await user.click(elements.submitBtn);
-
-  expect(elements.submitBtn).toBeEnabled();
-  expect(elements.rssInput).toHaveValue('Petya');
-  expect(elements.rssInput).toHaveClass('border', 'border-danger', 'is-invalid');
-  expect(elements.feedbackContainer).toHaveTextContent('Ссылка должна быть валидным URL');
-  expect(elements.feedbackContainer).toHaveClass('text-danger');
-
-  await user.clear(elements.rssInput);
-  await user.type(elements.rssInput, 'https://github.com/news.rss');
-  await user.click(elements.submitBtn);
-
-  expect(elements.submitBtn).toBeEnabled();
-  expect(elements.rssInput).toHaveValue('');
-  expect(elements.rssInput).toHaveFocus();
-  expect(elements.rssInput).not.toHaveClass('border', 'border-danger', 'is-invalid');
-  expect(elements.feedbackContainer).toHaveTextContent('RSS успешно загружен');
-  expect(elements.feedbackContainer).not.toHaveClass('text-danger');
-
-  await user.type(elements.rssInput, 'https://github.com/news.rss');
-  await user.click(elements.submitBtn);
-
-  expect(elements.submitBtn).toBeEnabled();
-  expect(elements.rssInput).toHaveValue('https://github.com/news.rss');
-  expect(elements.rssInput).toHaveClass('border', 'border-danger', 'is-invalid');
-  expect(elements.feedbackContainer).toHaveTextContent('RSS уже существует');
-  expect(elements.feedbackContainer).toHaveClass('text-danger');
 
   await waitFor(() => {
+    expect(elements.submitBtn).toBeEnabled();
+    expect(elements.rssInput).toHaveValue('');
+    expect(elements.rssInput).toHaveClass('border', 'border-danger', 'is-invalid');
+    expect(elements.feedbackContainer).toHaveTextContent('Не должно быть пустым');
+    expect(elements.feedbackContainer).toHaveClass('text-danger');
+  });
 
+  await user.type(elements.rssInput, 'Petya');
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.submitBtn).toBeEnabled();
+    expect(elements.rssInput).toHaveClass('border', 'border-danger', 'is-invalid');
+    expect(elements.feedbackContainer).toHaveTextContent('Ссылка должна быть валидным URL');
+    expect(elements.feedbackContainer).toHaveClass('text-danger');
+    expect(elements.rssInput).toHaveValue('Petya');
+  });
+
+  await user.clear(elements.rssInput);
+  await user.type(elements.rssInput, 'https://example.com/news');
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.submitBtn).toBeEnabled();
+    expect(elements.rssInput).toHaveValue('https://example.com/news');
+    expect(elements.rssInput).not.toHaveClass('border', 'border-danger', 'is-invalid');
+    expect(elements.feedbackContainer).toHaveTextContent('Ресурс не содержит валидный RSS');
+    expect(elements.feedbackContainer).toHaveClass('text-danger');
+  });
+
+  await user.clear(elements.rssInput);
+  await user.type(elements.rssInput, 'https://lorem-rss.herokuapp.com/feed');
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.submitBtn).toBeEnabled();
+    expect(elements.rssInput).toHaveValue('');
+    expect(elements.rssInput).toHaveFocus();
+    expect(elements.rssInput).not.toHaveClass('border', 'border-danger', 'is-invalid');
+    expect(elements.feedbackContainer).toHaveTextContent('RSS успешно загружен');
+    expect(elements.feedbackContainer).not.toHaveClass('text-danger');
+  });
+
+  await user.clear(elements.rssInput);
+  await user.type(elements.rssInput, 'https://lorem-rss.herokuapp.com/feed');
+  await user.click(elements.submitBtn);
+
+  await waitFor(() => {
+    expect(elements.submitBtn).toBeEnabled();
+    expect(elements.rssInput).toHaveValue('https://lorem-rss.herokuapp.com/feed');
+    expect(elements.rssInput).toHaveClass('border', 'border-danger', 'is-invalid');
+    expect(elements.feedbackContainer).toHaveTextContent('RSS уже существует');
+    expect(elements.feedbackContainer).toHaveClass('text-danger');
   });
 });
